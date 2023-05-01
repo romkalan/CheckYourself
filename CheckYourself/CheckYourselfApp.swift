b0f4441dbcd66e16205614b7306687e3feaffaf6
romkalan <romkalan@mail.ru>
2023-05-01 12:13:58 +0300

    Initial Commit

---
+++ CheckYourself/CheckYourselfApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckYourselfApp.swift
+//  CheckYourself
+//
+//  Created by Roman Lantsov on 01.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CheckYourselfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheckYourself/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CheckYourself
+//
+//  Created by Roman Lantsov on 01.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
