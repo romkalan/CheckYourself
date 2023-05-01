f6bb0b0cb7e032492a3f53f72546dcf3637c5493
romkalan <romkalan@mail.ru>
2023-05-01 15:12:16 +0300

    add UISlider from UIKit with opacity variable

--- CheckYourself/ContentView.swift
+++ CheckYourself/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showAlert = false
+    @State private var thumbOpacity = 0.0
+    @State private var currentValue = 50.0
+    @State private var targetValue = Int.random(in: 0...100)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 10) {
+            Text("Подвиньте слайдер как можно ближе к: \(targetValue)")
+            HStack {
+                Text("0")
+                UISliderRepresentation(
+                    currentValue: $currentValue,
+                    thumbOpacity: thumbOpacity
+                )
+                    .onChange(of: currentValue) { _ in
+                        setThumbOpacity()
+                    }
+                Text("100")
+            }
+            Button("Проверь меня!", action: { showAlert = true })
+                .alert("Ваш результат:", isPresented: $showAlert, actions: {}) {
+                    Text("\(computeScore())")
+                }
+            Button("Начать заново", action: {
+                targetValue = Int.random(in: 0...100)
+                setThumbOpacity()
+            })
         }
         .padding()
+        .onAppear {
+            setThumbOpacity()
+        }
+    }
+    
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - lround(currentValue))
+        return 100 - difference
+    }
+    
+    private func setThumbOpacity() {
+        thumbOpacity = Double(computeScore()) / 100
     }
 }
 
---
+++ CheckYourself/UISliderRepresentation.swift
@@ -0,0 +1,58 @@
+//
+//  UISliderRepresentation.swift
+//  CheckYourself
+//
+//  Created by Roman Lantsov on 01.05.2023.
+//
+
+import SwiftUI
+
+struct UISliderRepresentation: UIViewRepresentable {
+    @Binding var currentValue: Double
+    let thumbOpacity: Double
+    
+    func makeUIView(context: Context) -> UISlider {
+        let slider = UISlider()
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.value = Float(currentValue)
+        slider.thumbTintColor = .red
+        slider.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.changeSliderValue),
+            for: .valueChanged
+        )
+        
+        return slider
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        uiView.thumbTintColor = UIColor(red: 1, green: 0, blue: 0, alpha: thumbOpacity)
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(currentValue: $currentValue)
+    }
+}
+
+//MARK: - Coordinator
+extension UISliderRepresentation {
+    final class Coordinator: NSObject {
+        @Binding var currentValue: Double
+        
+        init(currentValue: Binding<Double>) {
+            self._currentValue = currentValue
+        }
+        
+        @objc func changeSliderValue(_ sender: UISlider) {
+            currentValue = Double(sender.value)
+        }
+        
+    }
+}
+
+struct UISliderRepresentation_Previews: PreviewProvider {
+    static var previews: some View {
+        UISliderRepresentation(currentValue: .constant(10), thumbOpacity: 0.5)
+    }
+}
